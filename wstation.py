7495b21295d2b72d2ee07ac3069591656aac37a2
Will <will.misc.acc@gmail.com>
2015-07-13 21:38:48 +0100

    Add rain gauge measurement

--- wstation.py
+++ wstation.py
@@ -35,14 +35,15 @@ if GLOBAL_BROADCOM_REF == True:
     GLOBAL_Pin_11   = 17
     GLOBAL_Pin_12   = 18
     GLOBAL_Pin_13   = 27
+    GLOBAL_Pin_14   = 27
 else:
     GLOBAL_Pin_11   = 11
     GLOBAL_Pin_12   = 12
     GLOBAL_Pin_13   = 13
-
+    GLOBAL_Pin_14   = 14
 
 # --- System set up ---
-GLOBAL_update_rate          = 3 # seconds
+GLOBAL_update_rate          = 5 # seconds
 GLOBAL_w1_device_path       = '/sys/bus/w1/devices/'
 GLOBAL_screen_output        = False
 
@@ -74,8 +75,16 @@ GLOBAL_in_temp_TS_field     = GLOBAL_thingspeak_field_2
 GLOBAL_in_hum_TS_field      = GLOBAL_thingspeak_field_3
 
 GLOBAL_door_sensor_enable   = True
-GLOBAL_door_sensor_pin      = GLOBAL_Pin_13
+GLOBAL_door_sensor_pin      = GLOBAL_Pin_14
 GLOBAL_door_TS_field        = GLOBAL_thingspeak_field_4
+
+GLOBAL_rain_sensor_enable   = True
+GLOBAL_rain_sensor_pin      = GLOBAL_Pin_13
+GLOBAL_rain_TS_field        = GLOBAL_thingspeak_field_5
+GLOBAL_rain_tick_measure    = 1.5 #millimeters
+GLOBAL_rain_tick_meas_time  = 0.5 #minutes
+GLOBAL_rain_tick_count      = 0
+GLOBAL_rain_task_count      = 0
 
 
 # --- Set up flashing LED ----
@@ -95,17 +104,35 @@ def setup_hardware():
     global s
     global GLOBAL_in_sensor_pin
     global GLOBAL_door_sensor_pin
-
+    global GLOBAL_rain_sensor_pin
+    
     s = DHT22.sensor(pi, GLOBAL_in_sensor_pin)
 
+    #Set up rain sensor input pin
+    pi.set_mode(GLOBAL_rain_sensor_pin, pigpio.INPUT)
+    rain_gauge = pi.callback(GLOBAL_rain_sensor_pin, pigpio.RISING_EDGE, count_rain_ticks)
+
     #Set up door sensor input pin
-    pi.set_mode(GLOBAL_door_sensor_pin, pigpio.INPUT) #Set up LED pin in
+    pi.set_mode(GLOBAL_door_sensor_pin, pigpio.INPUT)
     
     #Set up LED flashing thread
     pi.set_mode(GLOBAL_LED_pin, pigpio.OUTPUT) #Set up LED pin out
     timerThread = threading.Thread(target=toggle_LED)
     timerThread.daemon = True
     timerThread.start()
+
+
+#=======================================================================
+# EDGE CALLBACK FUNCTION TO COUNT RAIN TICKS
+#=======================================================================
+def count_rain_ticks(gpio, level, tick):
+    
+    global GLOBAL_rain_tick_count
+    
+    GLOBAL_rain_tick_count += 1
+    print(GLOBAL_rain_tick_count)
+    time.sleep(0.1) #debounce
+    
 
 
 #=======================================================================
@@ -278,10 +305,13 @@ def main():
     global GLOBAL_in_sensor_enable
     global GLOBAL_in_sensor_ref
     global GLOBAL_in_hum_sensor_enable
+    global GLOBAL_door_sensor_enable
+    global GLOBAL_rain_sensor_enable
+    global GLOBAL_rain_tick_count
+    global GLOBAL_rain_tick_meas_time
     global GLOBAL_thingspeak_enable_update
     global GLOBAL_update_rate
     global GLOBAL_LED_display_time
-    global GLOBAL_door_sensor_enable
     global GLOBAL_screen_output
 
 
@@ -293,6 +323,9 @@ def main():
 
         if '--insensor=OFF' in sys.argv:
             GLOBAL_in_sensor_enable = False
+
+        if '--rainsensor=OFF' in sys.argv:
+            GLOBAL_rain_sensor_enable = False
 
         if '--thingspeak=OFF' in sys.argv:
             GLOBAL_thingspeak_enable_update = False
@@ -308,6 +341,7 @@ def main():
             print('')
             print('   --outsensor=OFF    - disables outside temperature monitoring')
             print('   --insensor=OFF     - disables inside temperature monitoring')
+            print('   --rainsensor=OFF   - disables rainfall monitoring')
             print('   --thingspeak=OFF   - disable update to ThingSpeak')
             print('   --LEDtime=ON       - enables printing of LED toggle time')
             sys.exit(0)
@@ -320,10 +354,15 @@ def main():
     outside_temp    = 0
     inside          = {'temp':0 , 'hum':0}
     door_open       = 0
-
+    GLOBAL_rain_tick_meas_time = (GLOBAL_rain_tick_meas_time * 60) / GLOBAL_update_rate #convert from minutes to no. of tasks
+    GLOBAL_rain_tick_count = 0
+    GLOBAL_rain_task_count = 0
+    
     sensor_data     = []
     sensors         = []
 
+
+    #Prepare sensor list
     if GLOBAL_out_sensor_enable == True:
         sensors.append('outside temp')
 
@@ -333,6 +372,9 @@ def main():
         
     if GLOBAL_door_sensor_enable == True:
         sensors.append('door open')
+        
+    if GLOBAL_rain_sensor_enable == True:
+        sensors.append('rainfall')
 
     #Prepare thingspeak data to match sensor number
     for i in range(0, len(sensors)):
@@ -348,6 +390,16 @@ def main():
     #Main code
     try:
         while True:
+            
+            #Get rain fall measurement
+            if GLOBAL_out_sensor_enable == True:
+                if GLOBAL_rain_task_count == GLOBAL_rain_tick_meas_time:
+                    sensor_data[GLOBAL_rain_TS_field-1] = GLOBAL_rain_tick_count * GLOBAL_rain_tick_measure
+                    GLOBAL_rain_tick_count = 0
+                    GLOBAL_rain_task_count = 0
+                else:
+                    GLOBAL_rain_task_count += 1
+                    print(GLOBAL_rain_task_count)
 
             #Check door status
             if GLOBAL_door_sensor_enable == True:
@@ -358,7 +410,7 @@ def main():
             if GLOBAL_out_sensor_enable == True:
                 outside_temp = get_ds18b20_temp(GLOBAL_out_temp_sensor_ref)
                 sensor_data[GLOBAL_out_temp_TS_field-1] = outside_temp
-
+                
             #Get inside temperature and humidity
             if GLOBAL_in_sensor_enable == True:
                 inside = get_dht22_data()
