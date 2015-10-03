4f065d2744820844af90ab62c3b541dcae4fef78
Will <will.misc.acc@gmail.com>
2015-10-03 21:23:57 +0100

    Split code: read_sensors.py

---
+++ read_sensors.py
@@ -0,0 +1,219 @@
+#-------------------------------------------------------------------------------
+#
+# 'Controls shed weather station
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 William De Freitas
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+#-------------------------------------------------------------------------------
+
+#!/usr/bin/env python
+
+'''Gathers data from various sensors to capture weather conditions in shed.'''
+
+
+#===============================================================================
+# Import modules
+#===============================================================================
+
+# Standard Library
+import os
+import sys
+import time
+import datetime
+import logging
+
+# Third party modules
+import rrdtool
+import pigpio
+import DHT22
+
+# Application modules
+import DS18B20
+import settings as s
+
+
+#===============================================================================
+# Set up logger
+#===============================================================================
+log_directory = 'logs'
+log_file = 'read_sensors.log'
+
+if not os.path.exists(log_directory):
+    os.makedirs(log_directory)
+
+logging.basicConfig(filename='{directory}/{file_name}'.format(
+                                directory=log_directory, 
+                                file_name=log_file), 
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(name)s: %(message)s')
+logger = logging.getLogger(__name__)
+logger.info('--- Read Sensor Script Started ---')
+script_start_time = datetime.datetime.now()
+logger.info('Script start time: {start_time}'.format(
+    start_time=script_start_time.strftime('%Y-%m-%d %H:%M:%S'))) 
+
+
+#===============================================================================
+# MAIN
+#===============================================================================
+def main():
+    
+    '''Entry point for script'''
+
+    #Set initial variable values
+    out_sensor_enable            = True
+    in_sensor_enable             = True
+    thingspeak_enable_update     = True
+    door_sensor_enable           = True
+    rrdtool_enable_update        = True
+
+    sensors={
+        s.OUT_TEMP_NAME:0,
+        s.IN_TEMP_NAME:0,
+        s.IN_HUM_NAME:0,
+        s.DOOR_NAME:0
+    }
+
+
+    #---------------------------------------------------------------------------
+    # Load PIGPIO
+    #---------------------------------------------------------------------------
+    try:
+        pi = pigpio.pi()
+    except ValueError:
+        print('Failed to connect to PIGPIO')
+        logger.error('Failed to connect to PIGPIO ({value_error})'.format(
+            value_error=ValueError))
+        logger.info('Exiting...')
+        sys.exit()
+
+
+    #-------------------------------------------------------------------
+    # Get inside temperature and humidity
+    #-------------------------------------------------------------------
+    if in_sensor_enable:
+        logger.info('Reading value from DHT22 sensor')
+
+        #Set up sensor  
+        try:
+            DHT22_sensor = DHT22.sensor(pi, s.IN_SENSOR_PIN)
+        except ValueError:
+            print('Failed to connect to DHT22')
+            logger.error('Failed to connect to DHT22 ({value_error})'.format(
+                value_error=ValueError))
+            logger.info('Exiting...')
+            sys.exit()
+
+        #Read sensor
+        DHT22_sensor.trigger()
+        time.sleep(0.2)  #Do not over poll DHT22
+        sensors[s.IN_TEMP_NAME] = DHT22_sensor.temperature()
+        sensors[s.IN_HUM_NAME]  = DHT22_sensor.humidity() 
+
+   
+    #---------------------------------------------------------------------------
+    # SET UP RRD DATA AND TOOL
+    #---------------------------------------------------------------------------
+    if rrdtool_enable_update:
+        #Set up inital values for variables
+        rra_files        = []
+  
+               
+        #Create RRD files if none exist
+        if not os.path.exists(s.RRDTOOL_RRD_FILE):
+            logger.info('RRD file not found')
+            logger.info('Exiting...')
+            sys.exit()
+        else:
+            #Fetch data from round robin database & extract next entry time to sync loop
+            logger.info('RRD file found')
+            data_values = rrdtool.fetch(s.RRDTOOL_RRD_FILE, 'LAST', 
+                                        '-s', str(s.UPDATE_RATE * -2))
+            next_reading  = data_values[0][1]
+            logger.info('RRD FETCH: Next sensor reading at {time}'.format(
+                time=time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(next_reading))))
+
+
+    #-------------------------------------------------------------------
+    # Check door status
+    #-------------------------------------------------------------------
+    if door_sensor_enable:
+        logger.info('Reading value from door sensor')
+
+        #Set up hardware
+        pi.set_mode(s.DOOR_SENSOR_PIN, pigpio.INPUT)
+
+        #Read data
+        sensors[s.DOOR_NAME] = pi.read(s.DOOR_SENSOR_PIN)
+
+
+    #-------------------------------------------------------------------
+    # Get outside temperature
+    #-------------------------------------------------------------------
+    if out_sensor_enable:
+        logger.info('Reading value from DS18B20 sensor')
+        sensors[s.OUT_TEMP_NAME] = DS18B20.get_temp(s.W1_DEVICE_PATH, 
+                                                    s.OUT_TEMP_SENSOR_REF)
+
+
+    #-------------------------------------------------------------------
+    # Display data on screen
+    #-------------------------------------------------------------------
+    print(sensors)
+
+
+    #-------------------------------------------------------------------
+    # Add data to RRD
+    #-------------------------------------------------------------------
+    if rrdtool_enable_update:
+        logger.info('Updating RRD file')
+
+        sensor_data = []
+        for key, value in sorted(sensors.items()):
+            sensor_data.append(value)
+        sensor_data = [str(i) for i in sensor_data]
+
+        try:
+            rrdtool.update(s.RRDTOOL_RRD_FILE, 'N:' + ':'.join(sensor_data))
+        except rrdtool.error:
+            logger.error('Failed to update RRD file ({value_error})'.format(
+                value_error=rrdtool.error))
+
+
+    #-------------------------------------------------------------------
+    # Prepare to end script
+    #-------------------------------------------------------------------
+    #Stop processes
+    DHT22_sensor.cancel()
+    
+    logger.info('--- Read Sensors Finished ---')
+
+    sys.exit()
+
+        
+
+#===============================================================================
+# BOILER PLATE
+#===============================================================================
+if __name__=='__main__':
+    sys.exit(main())
