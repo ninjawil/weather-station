c65df74acfbd11ec4afeaad3c67b29e8ffae0d9b
ninjawil <ninjawil@users.noreply.github.com>
2015-10-06 16:31:27 +0100

    Update wstation.py

--- wstation.py
+++ wstation.py
@@ -39,7 +39,6 @@ apropriate actions in shed.'''
 # Standard Library
 import os
 import sys
-import threading
 import time
 import datetime
 import logging
@@ -63,8 +62,8 @@ if not os.path.exists(log_directory):
 logging.basicConfig(filename='{directory}/{file_name}'.format(
                                 directory=log_directory, 
                                 file_name=log_file), 
-    level=logging.INFO,
-    format='%(asctime)s [%(levelname)s] %(name)s: %(message)s')
+                    level=logging.INFO,
+                    format='%(asctime)s [%(levelname)s] %(name)s: %(message)s')
 logger = logging.getLogger(__name__)
 logger.info('--- Read Rain Gauge Script Started ---')
 script_start_time = datetime.datetime.now()
@@ -72,21 +71,47 @@ logger.info('Script start time: {start_time}'.format(
     start_time=script_start_time.strftime('%Y-%m-%d %H:%M:%S'))) 
 
 
-
-
 #===============================================================================
-# TOGGLE LED
+# CREATE RRD FILE
 #===============================================================================
-def create_rrd_data_source(source_name, source_type, source_heartbeat, 
-                            source_min, source_max):
-    return 'DS:{ds_name}:{ds_type}:{ds_hb}:{ds_min}:{ds_max}'.format(
-        ds_name=source_name.replace(' ','_'),
-        ds_type=source_type,
-        ds_hb=source_heartbeat,
-        ds_min=source_min,
-        ds_max=source_max)
-
+def create_rrd_file(file_dir, file_name, sensor_set, rra_set, update_rate, 
+                    heartbeat, start_time):
     
+    '''Creates a RRD file'''
+
+    #Check if directory exists
+    if not os.path.exists(file_dir):
+        os.makedirs(file_dir)
+
+    #Prepare data sources
+    rrd_ds      = []
+    for i in sorted(sensor_set):
+        rrd_ds.append('DS:{ds_name}:{ds_type}:{ds_hb}:{ds_min}:{ds_max}'.format(
+                                ds_name=i,
+                                ds_type=sensor_set[i][5],
+                                ds_hb=str(heartbeat*update_rate),
+                                ds_min=sensor_set[i][3],
+                                ds_max=sensor_set[i][4]))
+
+    #Prepare RRA files
+    rra_files   = []
+    for i in range(0,len(rra_set),3):
+        rra_files.append('RRA:{cf}:0.5:{steps}:{rows}'.format(
+                                cf=rra_set[i],
+                                steps=str((rra_set[i+1]*60)/update_rate),
+                                rows=str(((rra_set[i+2])*24*60)/rra_set[i+1])))
+
+    #Prepare RRD set
+    rrd_set = []
+    rrd_set = [file_name, 
+                '--step', '{step}'.format(step=update_rate), 
+                '--start', '{start_t:.0f}'.format(start_t=start_time)]
+    rrd_set +=  rrd_ds + rra_files
+
+    rrdtool.create(rrd_set)
+
+    return rrd_set
+
 
 #===============================================================================
 # MAIN
@@ -94,107 +119,34 @@ def create_rrd_data_source(source_name, source_type, source_heartbeat,
 def main():
     
     '''Entry point for script'''
-
-
-    #Set initial variable values
-    rain_sensor_enable           = True
-    out_sensor_enable            = True
-    in_sensor_enable             = True
-    thingspeak_enable_update     = True
-    door_sensor_enable           = True
-    
-    rrd_data_sources             = []
-    rrd_set                      = []
-
-    
-    #---------------------------------------------------------------------------
-    # SET UP OUTSIDE TEMPERATURE SENSOR
-    #---------------------------------------------------------------------------
-    if out_sensor_enable:
-        rrd_data_sources += [create_rrd_data_source(s.OUT_TEMP_NAME, 
-                                                    s.OUT_TEMP_TYPE,
-                                                    str(s.RRDTOOL_HEARTBEAT*s.UPDATE_RATE),
-                                                    str(s.OUT_TEMP_MIN),
-                                                    str(s.OUT_TEMP_MAX))]
-
-
-    #---------------------------------------------------------------------------
-    # SET UP INSIDE TEMPERATURE SENSOR
-    #---------------------------------------------------------------------------
-    if in_sensor_enable:
-        rrd_data_sources += [create_rrd_data_source(s.IN_TEMP_NAME, 
-                                                    s.IN_TEMP_TYPE,
-                                                    str(s.RRDTOOL_HEARTBEAT*s.UPDATE_RATE),
-                                                    str(s.IN_TEMP_MIN),
-                                                    str(s.IN_TEMP_MAX))]
-        rrd_data_sources += [create_rrd_data_source(s.IN_HUM_NAME, 
-                                                    s.IN_HUM_TYPE,
-                                                    str(s.RRDTOOL_HEARTBEAT*s.UPDATE_RATE),
-                                                    str(s.IN_HUM_MIN),
-                                                    str(s.IN_HUM_MAX))]
-
-    
-    #---------------------------------------------------------------------------
-    # SET UP DOOR SENSOR
-    #---------------------------------------------------------------------------
-    if door_sensor_enable:
-        rrd_data_sources += [create_rrd_data_source(s.DOOR_NAME, 
-                                                    s.DOOR_TYPE,
-                                                    str(s.RRDTOOL_HEARTBEAT*s.UPDATE_RATE),
-                                                    str(s.DOOR_MIN),
-                                                    str(s.DOOR_MAX))]
-
-
-    #---------------------------------------------------------------------------
-    # SET UP RAIN SENSOR
-    #---------------------------------------------------------------------------
-    if rain_sensor_enable:
-        rrd_data_sources += [create_rrd_data_source(s.PRECIP_RATE_NAME, 
-                                                    s.PRECIP_RATE_TYPE,
-                                                    str(s.RRDTOOL_HEARTBEAT*s.UPDATE_RATE),
-                                                    str(s.PRECIP_RATE_MIN),
-                                                    str(s.PRECIP_RATE_MAX))]
-        rrd_data_sources += [create_rrd_data_source(s.PRECIP_ACCU_NAME, 
-                                                    s.PRECIP_ACCU_TYPE,
-                                                    str(s.RRDTOOL_HEARTBEAT*s.UPDATE_RATE),
-                                                    str(s.PRECIP_ACCU_MIN),
-                                                    str(s.PRECIP_ACCU_MAX))]                               
-
-
+ 
     #---------------------------------------------------------------------------
     # SET UP RRD DATA AND TOOL
     #---------------------------------------------------------------------------
-    if rrdtool_enable_update:
-        #Set up inital values for variables
-        rra_files        = []
-  
-        #Prepare RRA files
-        for i in range(0,len(s.RRDTOOL_RRA),3):
-            rra_files.append('RRA:{cf}:0.5:{steps}:{rows}'.format(
-                cf=s.RRDTOOL_RRA[i],
-                steps=str((s.RRDTOOL_RRA[i+1]*60)/s.UPDATE_RATE),
-                rows=str(((s.RRDTOOL_RRA[i+2])*24*60)/s.RRDTOOL_RRA[i+1])))
- 
-        #Prepare RRD set
-        rrd_set = [s.RRDTOOL_RRD_FILE, 
-                    '--step', '{step}'.format(step=s.UPDATE_RATE), 
-                    '--start', '{start_time:.0f}'.format(start_time=next_reading)]
-        rrd_set +=  rrd_data_sources + rra_files
-        
-        #Create RRD files if none exist
-        if not os.path.exists(s.RRDTOOL_RRD_FILE):
-            logger.info('RRD file not found')
-            logger.info(rrd_set)
-            rrdtool.create(rrd_set)
-            logger.info('New RRD file created')
-        else:
-            #Fetch data from round robin database & extract next entry time to sync loop
-            logger.info('RRD file found')
-            data_values = rrdtool.fetch(s.RRDTOOL_RRD_FILE, 'LAST', 
-                                        '-s', str(s.UPDATE_RATE * -2))
-            next_reading  = data_values[0][1]
-            logger.info('RRD FETCH: Next sensor reading at {time}'.format(
-                time=time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(next_reading))))
+
+    #Create RRD files if none exist
+    if not os.path.exists('{directory}/{file_name}'.format(
+                                        directory=s.RRDTOOL_RRD_DIR, 
+                                        file_name=s.RRDTOOL_RRD_FILE)):
+
+        logger.info('RRD file not found')
+        logger.info(create_rrd_file(s.RRDTOOL_RRD_DIR, 
+                                    s.RRDTOOL_RRD_FILE,
+                                    s.SENSOR_SET,
+                                    s.RRDTOOL_RRA, 
+                                    s.UPDATE_RATE, 
+                                    s.RRDTOOL_HEARTBEAT,
+                                    datetime.datetime.now() + s.UPDATE_RATE))
+        logger.info('New RRD file created')
+
+    else:
+        #Fetch data from round robin database & extract next entry time to sync loop
+        logger.info('RRD file found')
+        data_values = rrdtool.fetch(s.RRDTOOL_RRD_FILE, 'LAST', 
+                                    '-s', str(s.UPDATE_RATE * -2))
+        next_reading  = data_values[0][1]
+        logger.info('RRD FETCH: Next sensor reading at {time}'.format(
+            time=time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(next_reading))))
 
 
 #===============================================================================
