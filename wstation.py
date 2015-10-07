af18adbb37925746ef57b52d612584652593e329
ninjawil <ninjawil@users.noreply.github.com>
2015-10-07 12:13:54 +0100

    Update for rrd_tool.py

--- wstation.py
+++ wstation.py
@@ -48,6 +48,7 @@ import rrdtool
 
 # Application modules
 import settings as s
+import rrd_tools as r
 
 
 #===============================================================================
@@ -71,47 +72,6 @@ logger.info('Script start time: {start_time}'.format(
     start_time=script_start_time.strftime('%Y-%m-%d %H:%M:%S'))) 
 
 
-#===============================================================================
-# CREATE RRD FILE
-#===============================================================================
-def create_rrd_file(file_dir, file_name, sensor_set, rra_set, update_rate, 
-                    heartbeat, start_time):
-    
-    '''Creates a RRD file'''
-
-    #Check if directory exists
-    if not os.path.exists(file_dir):
-        os.makedirs(file_dir)
-
-    #Prepare data sources
-    rrd_ds      = []
-    for i in sorted(sensor_set):
-        rrd_ds.append('DS:{ds_name}:{ds_type}:{ds_hb}:{ds_min}:{ds_max}'.format(
-                                ds_name=i,
-                                ds_type=sensor_set[i][5],
-                                ds_hb=str(heartbeat*update_rate),
-                                ds_min=sensor_set[i][3],
-                                ds_max=sensor_set[i][4]))
-
-    #Prepare RRA files
-    rra_files   = []
-    for i in range(0,len(rra_set),3):
-        rra_files.append('RRA:{cf}:0.5:{steps}:{rows}'.format(
-                                cf=rra_set[i],
-                                steps=str((rra_set[i+1]*60)/update_rate),
-                                rows=str(((rra_set[i+2])*24*60)/rra_set[i+1])))
-
-    #Prepare RRD set
-    rrd_set = []
-    rrd_set = [file_name, 
-                '--step', '{step}'.format(step=update_rate), 
-                '--start', '{start_t:.0f}'.format(start_t=start_time)]
-    rrd_set +=  rrd_ds + rra_files
-
-    rrdtool.create(rrd_set)
-
-    return rrd_set
-
 
 #===============================================================================
 # MAIN
@@ -130,7 +90,7 @@ def main():
                                         file_name=s.RRDTOOL_RRD_FILE)):
 
         logger.info('RRD file not found')
-        logger.info(create_rrd_file(s.RRDTOOL_RRD_DIR, 
+        logger.info(r.create_rrd_file(s.RRDTOOL_RRD_DIR, 
                                     s.RRDTOOL_RRD_FILE,
                                     s.SENSOR_SET,
                                     s.RRDTOOL_RRA, 
