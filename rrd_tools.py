76e3b83dfb64a73f24f6003714a0f2a01fa2d59e
ninjawil <ninjawil@users.noreply.github.com>
2015-10-13 14:49:44 +0100

    Add class

--- rrd_tools.py
+++ rrd_tools.py
@@ -1,7 +1,5 @@
 #-------------------------------------------------------------------------------
 #
-# 'Controls shed weather station
-#
 # The MIT License (MIT)
 #
 # Copyright (c) 2015 William De Freitas
@@ -37,88 +35,88 @@
 
 # Standard Library
 import os
-import sys
 import time
-import datetime
-import logging
 
 # Third party modules
 import rrdtool
 
 
+ #===============================================================================
+ class rrd_file:
+ 
+    '''Sets up the RRD file 'a thingspeak account'''
+ 
+    def __init__(self, filename):
+        self.file_name = filename
+ 
+ 
+    #---------------------------------------------------------------------------
+    # CREATE RRD FILE
+    #---------------------------------------------------------------------------
+    def create_file(self, sensor_set, rra_set, update_rate, heartbeat, start_time):
+        
+        '''Creates a RRD file'''
 
-#===============================================================================
-# CREATE RRD FILE
-#===============================================================================
-def create_file(file_dir, file_name, sensor_set, rra_set, update_rate, 
-                    heartbeat, start_time):
-    
-    '''Creates a RRD file'''
+        #Prepare data sources
+        rrd_ds      = []
+        for i in sorted(sensor_set):
+            rrd_ds.append('DS:{ds_name}:{ds_type}:{ds_hb}:{ds_min}:{ds_max}'.format(
+                                    ds_name=i,
+                                    ds_type=sensor_set[i][5],
+                                    ds_hb=str(heartbeat*update_rate),
+                                    ds_min=sensor_set[i][3],
+                                    ds_max=sensor_set[i][4]))
 
-    #Check if directory exists
-    if not os.path.exists(file_dir):
-        os.makedirs(file_dir)
+        #Prepare RRA files
+        rra_files   = []
+        for i in range(0,len(rra_set),3):
+            rra_files.append('RRA:{cf}:0.5:{steps}:{rows}'.format(
+                                    cf=rra_set[i],
+                                    steps=str((rra_set[i+1]*60)/update_rate),
+                                    rows=str(((rra_set[i+2])*24*60)/rra_set[i+1])))
 
-    #Prepare data sources
-    rrd_ds      = []
-    for i in sorted(sensor_set):
-        rrd_ds.append('DS:{ds_name}:{ds_type}:{ds_hb}:{ds_min}:{ds_max}'.format(
-                                ds_name=i,
-                                ds_type=sensor_set[i][5],
-                                ds_hb=str(heartbeat*update_rate),
-                                ds_min=sensor_set[i][3],
-                                ds_max=sensor_set[i][4]))
+        #Prepare RRD set
+        rrd_set = []
+        rrd_set = [self.file_name, 
+                    '--step', '{step}'.format(step=update_rate), 
+                    '--start', '{start_t:.0f}'.format(start_t=start_time)]
+        rrd_set +=  rrd_ds + rra_files
 
-    #Prepare RRA files
-    rra_files   = []
-    for i in range(0,len(rra_set),3):
-        rra_files.append('RRA:{cf}:0.5:{steps}:{rows}'.format(
-                                cf=rra_set[i],
-                                steps=str((rra_set[i+1]*60)/update_rate),
-                                rows=str(((rra_set[i+2])*24*60)/rra_set[i+1])))
+        rrdtool.create(rrd_set)
 
-    #Prepare RRD set
-    rrd_set = []
-    rrd_set = [file_name, 
-                '--step', '{step}'.format(step=update_rate), 
-                '--start', '{start_t:.0f}'.format(start_t=start_time)]
-    rrd_set +=  rrd_ds + rra_files
-
-    rrdtool.create(rrd_set)
-
-    return rrd_set
-
-
-#===============================================================================
-# UPDATE RRD FILE
-#===============================================================================
-def update_file(file_name,data_values):
-    try:
-        rrdtool.update(file_name, 'N:{values}'.format(
-            values=':'.join([str(data_values[i]) for i in sorted(data_values)])))
-        return 'OK'
-    except rrdtool.error, e:
-        return e
+        return rrd_set
 
 
-#===============================================================================
-# INFO
-#===============================================================================
-def info(file_name):
-    return rrdtool.info(file_name)
+    #---------------------------------------------------------------------------
+    # UPDATE RRD FILE
+    #---------------------------------------------------------------------------
+    def update_file(self, data_values):
+        try:
+            rrdtool.update(self.file_name, 'N:{values}'.format(
+                values=':'.join([str(data_values[i]) for i in sorted(data_values)])))
+            return 'OK'
+        except rrdtool.error, e:
+            return e
 
 
-#===============================================================================
-# DS LIST
-#===============================================================================
-def ds_list(file_name):
-    info =  info(file_name)
-    return info[1]
+    #---------------------------------------------------------------------------
+    # INFO
+    #---------------------------------------------------------------------------
+    def info(self):
+        return rrdtool.info(self.file_name)
 
 
-#===============================================================================
-# LAST UPDATE
-#===============================================================================
-def last_update(file_name):
-    info =  info(file_name)
-    return info['last_update']
+    #---------------------------------------------------------------------------
+    # DS LIST
+    #---------------------------------------------------------------------------
+    def ds_list(self):
+        info =  info(self.file_name)
+        return info[1]
+
+
+    #---------------------------------------------------------------------------
+    # LAST UPDATE
+    #---------------------------------------------------------------------------
+    def last_update(self):
+        info =  info(self.file_name)
+        return info['last_update']
