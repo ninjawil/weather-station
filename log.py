78bd0cb1a11fb685b4763c393ea2d16ccb040a66
ninjawil <ninjawil@users.noreply.github.com>
2015-10-14 15:32:15 +0100

    Create log.py

---
+++ log.py
@@ -0,0 +1,63 @@
+#-------------------------------------------------------------------------------
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 William De Freitas
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+#-------------------------------------------------------------------------------
+
+#!/usr/bin/env python
+
+'''Provides a common logging set up for all scripts'''
+
+
+#===============================================================================
+# Import modules
+#===============================================================================
+import logging
+
+
+#===============================================================================
+# Custom logger
+#===============================================================================
+def setup(name, log_file):
+   
+    formatter = logging.Formatter(
+    	fmt='%(asctime)s [%(levelname)-8s] %(module)-15s : %(message)s')
+
+        
+    fh = logging.handlers.TimedRotatingFileHandler(filename=log_file, 
+                                                    when='midnight', 
+                                                    backupCount=7, 
+                                                    utc=True)
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.ERROR)
+    ch.setFormatter(formatter)
+    
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
+    return logger
