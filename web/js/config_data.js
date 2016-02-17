162a5a6a32ea96060b2ef9e81c661253787bada0
Will <will.misc.acc@gmail.com>
2016-02-17 21:31:59 +0000

    Add more settings input boxes

--- web/js/config_data.js
+++ web/js/config_data.js
@@ -16,12 +16,42 @@ function grabConfigData(){
 function updateSettingsModal(config_data) {
 
 	if(config_data.heater.HEATER_ENABLE == 1){
-		$('#heater-enable').attr('placeholder', 'checked');		
+		$('#heater-enable').prop('checked', true);
+		$('#heater-on-temp').prop('disabled', false);
+		$('#heater-on-hyst').prop('disabled', false);
+		$('#miplug-id').prop('disabled', false);
+	} else {
+		$('#heater-enable').prop('checked', false);
+		$('#heater-on-temp').prop('disabled', true);
+		$('#heater-on-hyst').prop('disabled', true);
+		$('#miplug-id').prop('disabled', true);	
 	}
 
 	$('#heater-on-temp').attr('placeholder', config_data.heater.TEMP_HEATER_ON);
 	$('#heater-on-hyst').attr('placeholder', config_data.heater.TEMP_HYSTERISIS);
 	$('#miplug-id').attr('placeholder', config_data.heater.MIPLUG_SENSOR_ID);
+
+	$('#rain-gauge').attr('placeholder', config_data.rain_gauge.PRECIP_TICK_MEASURE);
+
+	$('#ts-host-addr').attr('placeholder', config_data.thingspeak.THINGSPEAK_HOST_ADDR);
+	$('#ts-api-key').attr('placeholder', config_data.thingspeak.THINGSPEAK_API_KEY);
+	$('#ts-ch-id').attr('placeholder', config_data.thingspeak.THINGSPEAK_CHANNEL_ID);
+
+
+	$(function(){
+	    $('#chkboxes input:checkbox').on('change', function(){
+	        if($(this).is(':checked'))
+	        {
+	        	$('#heater-enable').prop('checked', true);
+	            $('#heater-on-temp').prop('disabled', false);
+	        }
+	        else
+	        {
+	            $('#heater-enable').prop('checked', false);
+	            $('#heater-on-temp').prop('disabled', true);
+	        }
+	    });​
+	});
 
 }
 
