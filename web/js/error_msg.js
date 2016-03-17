834b69605955cb3c33c0a4653b50785550202aae
Will <will.misc.acc@gmail.com>
2016-03-17 21:46:53 +0000

    Web page error display

--- web/js/error_msg.js
+++ web/js/error_msg.js
@@ -35,34 +35,27 @@ function displayErrorMessage(json) {
 
 	error_data = json;
 
-	if(error_data.error_1.active == 1)
-	{
-		errorValue = 1;
-		errorType = error_data.error_1.type;
-		errorMessage = error_data.error_1.message;
+	errorValue = 0;
+	errorMessage = '';
+
+	for (var error in error_data) {
+		if(error_data[error].time != 0) {
+			errorValue = error;
+			errorMessage = error_data[error].msg;
+		}
+	};
+
+	if(errorValue != 0){
+
+		var displayErrorType = "   Error E"+ pad(errorValue, 4) + ": ";
+
+		var formattedErrorMsg = HTMLerrorMSG.replace("%error_type%", displayErrorType);
+		formattedErrorMsg = formattedErrorMsg.replace("%error_msg%", errorMessage);
+
+		$('#error_display').append(formattedErrorMsg);
 	}
-
-	var displayErrorType = errorType + " E"+ pad(errorValue, 3) + ": ";
-
-	var formattedErrorMsg = HTMLerrorMSG.replace("%error_type%", displayErrorType);
-	formattedErrorMsg = formattedErrorMsg.replace("%error_msg%", errorMessage);
-
-	$('#error_display').append(formattedErrorMsg);
 }
 
-
-//-------------------------------------------------------------------------------
-// Displays next error
-//-------------------------------------------------------------------------------
-$(document).ready(function(){
-    $(".close").click(function(){
-        $("#myAlert").alert("close");
-    });
-    $("#myAlert").on('closed.bs.alert', function(){
-        console.log('closed and run again!');
-        grabConfigData();
-    });
-});
 
 //-------------------------------------------------------------------------------
 // Prepares data and displays it
