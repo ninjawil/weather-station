309d78f88dd6e0c469deca9f2a7dc7420b5e8f38
Will <will.misc.acc@gmail.com>
2016-04-12 20:37:30 +0100

    Correct script command

--- web/php/clear_errors.php
+++ web/php/clear_errors.php
@@ -1,4 +1,4 @@
 <?php
-$result = shell_exec('python /home/pi/weather/scripts/watchdog.py --clear');
+$result = shell_exec('/usr/bin/python /home/pi/weather/scripts/watchdog.py --clear');
 echo $result;
 ?>
