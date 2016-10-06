a09471116da16ee5a467a03d92d83d1300d205bf
ninjawil <will.misc.acc@gmail.com>
2016-10-06 15:55:55 +0100

    Add json read write note data

--- scripts/evernote_sync.py
+++ scripts/evernote_sync.py
@@ -24,6 +24,21 @@ import log
 import check_process
 
 
+#---------------------------------------------------------------------------
+# Get guid for tag
+#---------------------------------------------------------------------------
+def get_tag_guid(tag_list, search_string):
+    '''
+    Function returns the tag guid from a list
+
+    tag_list        Struct: Tag as defined by Evernote
+    search_string   string of tag name to search 
+
+    '''
+
+    return {tag.name: tag.guid for tag in tag_list if tag.name.find(search_string) > -1}
+
+
 
 #===============================================================================
 # MAIN
@@ -59,70 +74,103 @@ def main():
     #---------------------------------------------------------------------------
     # GET CONFIG & GARDENING DATA
     #---------------------------------------------------------------------------
-    # with open('{fl}/data/config.json'.format(fl= folder_loc), 'r') as f:
-    #     config = json.load(f)
+    try:
+        with open('{fl}/data/config.json'.format(fl= folder_loc), 'r') as f:
+            config = json.load(f)
+        with open('{fl}/data/key.json'.format(fl= folder_loc), 'r') as f:
+            key = json.load(f)
+    except Exception, e:
+        logger.error('Error ({error_v}). Exiting...'.format(error_v=e), exc_info=True)
+        sys.exit()
 
-    gardening_tag_name = '!gardening'
-
-    with open(self.file_loc, 'r') as f:
-            return json.load(f)
-
-
-
-
-    #---------------------------------------------------------------------------
-    # GET EVERNOTE DATA
-    #---------------------------------------------------------------------------
-    authToken = "S=s1:U=92f9a:E=15ee8b5fe6c:C=1579104cf10:P=1cd:A=en-devtoken:V=2:H=f5a3445b1ba2c095195f3491ef21d797"
-
-    client = EvernoteClient(token=authToken, sandbox=True)
-    
-    user_store = client.get_user_store()
-
-    version_ok = user_store.checkVersion(
-        "Evernote EDAMTest (Python)",
-        UserStoreConstants.EDAM_VERSION_MAJOR,
-        UserStoreConstants.EDAM_VERSION_MINOR
-    )
-
-    logger.info("Is my Evernote API version up to date? {response}".format(response= str(version_ok)))
-    if not version_ok:
-        exit(1)
-
-    note_store = client.get_note_store()
-
-    state = note_store.getSyncState()
-    logger.info(state)
+    try:
+        with open('{fl}/data/gardening.json'.format(fl= folder_loc), 'r') as f:
+            gardening_notes = json.load(f)
+    except Exception, e:
+        logger.warning('Error ({error_v}).'.format(error_v=e), exc_info=True)
+        gardening_notes = {'sync_updateCount': 0, 'tags': {}, 'notes': {}}
 
 
-    tags = note_store.listTags()
-
-    # for tag in tags:
-    #     logger.info(tag)
-
-    # Search for a tag
-    gardening_tag = [tag.guid for tag in tags if tag.name == gardening_tag_name]  
-    logger.info(gardening_tag)
-
-    filter = NoteStore.NoteFilter()
-    filter.tagGuids = gardening_tag
-
-    spec = NoteStore.NotesMetadataResultSpec()
-    spec.includeTitle = True
-    spec.includeCreated = True
-    spec.includeTagGuids = True
-
-    ourNoteList = note_store.findNotesMetadata(authToken, filter, 0, 10000, spec)
-
-    for notes in ourNoteList.notes:
-        logger.info(notes)
 
 
     #---------------------------------------------------------------------------
-    # WRITE EVERNOTE DATA TO JSON
+    # GET EVERNOTE DATA AND WRITE TO FILE
     #---------------------------------------------------------------------------
-    # with open('{fl}/data/gardening.json'.format(fl= folder_loc), 'w') as f:
-    #     json.dump(data, f)
+    try:
+
+        client = EvernoteClient(token=key['AUTH_TOKEN'], sandbox=True)
+        
+        note_store = client.get_note_store()
+        user_store = client.get_user_store()
+        user = user_store.getUser()
+
+
+        # Check evernote API
+        version_ok = user_store.checkVersion(
+            "Evernote EDAMTest (Python)",
+            UserStoreConstants.EDAM_VERSION_MAJOR,
+            UserStoreConstants.EDAM_VERSION_MINOR)
+
+        if not version_ok:
+            logger.warning("Evernote API version is not up to date.")
+
+
+        # Check sync state
+        state = note_store.getSyncState()
+        if state.updateCount <= gardening_notes['sync_updateCount']:
+            logger.info('Local file is in sync with Evernote. Exiting...')
+            # sys.exit()
+
+        gardening_notes['sync_updateCount'] = state.updateCount
+
+        logger.info('Syncing local data with Evernote...')
+
+
+        # Get all tags and search for a specific tag
+        tags            = note_store.listTags()
+        gardening_tag   = get_tag_guid(tags, config['evernote']['GARDENING_TAG'])
+        gardening_notes['tags']  = get_tag_guid(tags, config['evernote']['PLANT_TAG_ID'])
+
+
+        # Get all notes with specific tag
+        filter = NoteStore.NoteFilter()
+        filter.tagGuids = [gardening_tag[config['evernote']['GARDENING_TAG']]]
+
+        spec = NoteStore.NotesMetadataResultSpec()
+        spec.includeTitle = True
+        spec.includeCreated = True
+        spec.includeTagGuids = True
+
+        note_list = note_store.findNotesMetadata(key['AUTH_TOKEN'], filter, 0, 10000, spec)
+
+        for note in note_list.notes:
+            gardening_notes['notes'][note.guid] = {
+                'created':  note.created,
+                'title':    note.title,
+                'tags':     note.tagGuids,
+                'link':     'https://{service}/shard/{shardId}/nl/{userId}/{noteGuid}/'.format(
+                                    service= key['SERVICE'],
+                                    shardId= user.shardId,
+                                    userId= user.id,
+                                    noteGuid= note.guid)
+            }
+
+
+        # Write gardening data to 
+        with open('{fl}/data/gardening.json'.format(fl= folder_loc), 'w') as f:
+            json.dump(gardening_notes, f)
+
+
+        logger.info('Sync Complete')
+
+
+    except Exception, e:
+        logger.error('Script error ({error_v}). Exiting...'.format(
+            error_v=e), exc_info=True)
+        sys.exit()
+
+    finally:
+        logger.info('--- Script Finished ---')
 
 
 #===============================================================================
