a973f2668996be1acbd76b26b4e5910c98adc098
ninjawil <will.misc.acc@gmail.com>
2017-02-27 19:45:13 +0000

    Remove non-pi related files

--- web_pi/php/save_settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$filename = $_SERVER['DOCUMENT_ROOT'] . '/weather_data/config.json';
-	$decoded = base64_decode($_POST['json']);
-	$jsonFile = fopen($filename,'w') or die("Unable to open file!");
-	fwrite($jsonFile,$decoded);
-	fclose($jsonFile);
-	echo 'Data Saved';
-?>
